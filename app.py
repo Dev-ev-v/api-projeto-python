79d789d970e7afb9bc8c648dd336d35eae3b2ce5
Dev-ev-v <jogos.testedostestes@gmail.com>
2026-02-11 22:25:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+tarefas = []
+
+@app.route("/tarefas", methods=["GET"])
+def listar():
+    return jsonify({"tarefas": tarefas})
+
+@app.route("/tarefas", methods=["POST"])
+def adicionar():
+    dados = request.get_json()
+
+    if not dados or "tarefa" not in dados:
+        return jsonify({"erro": "campo 'tarefa' obrigatório"}), 400
+
+    tarefas.append(dados["tarefa"])
+    return jsonify({"status": "adicionado", "tarefa": dados["tarefa"]}), 201
